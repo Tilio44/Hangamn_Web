c09ee89ddb18eeaf93db14ce2aff8452af0fdb71
Tilio44 <112935656+Tilio44@users.noreply.github.com>
2022-11-25 11:15:41 +0100

    Update Dash.go

--- Dash.go
+++ Dash.go
@@ -4,7 +4,8 @@ package hangman
 func Dash(word string) string {
 	wordDash := []byte(word)
 	for j := 0; j < len(word); j++ {
-		wordDash[j] = 95 && 32
+		wordDash[j] += ("_");
+        	wordDash[j] += (" ");
 	}
 	return string(wordDash)
 }
